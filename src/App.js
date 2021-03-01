7c0978161f087157cc57189ebf975057b5c3c988
vincentservio <vincentservio@gmail.com>
2021-03-01 00:50:25 -0500

    xxxyyzzz

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react'
+import React, {Component} from "react";
 // import { connect } from 'react-redux'
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 // import { Switch} from 'react-router-dom';
-import Home from './components/Home'
-import TracksContainer from './container/TracksContainer'
-import Nav from './components/Nav'
-import TrackForm from "./container/TrackForm"
-import InstrumentalsContainer from './container/InstrumentalsContainter';
-import Footer from "./components/Footer"
- class App extends Component {
-
+import Home from "./components/Home";
+import TracksContainer from "./container/TracksContainer";
+import Nav from "./components/Nav";
+import TrackForm from "./container/TrackForm";
+import InstrumentalsContainer from "./container/InstrumentalsContainter";
+import Footer from "./components/Footer";
+class App extends Component {
   render() {
     return (
       <Router>
-        <Nav/>
-        <div className="App">             
-          <Switch>          
-            <Route path="/tracks/new" exact component={ TrackForm } />                 
-            <Route  path="/instrumentals" exact component={ InstrumentalsContainer } />
-            <Route  path="/tracks" exact component={ TracksContainer } />
-            <Route path="/" exact component={ Home }/>
-          </Switch >
+        <Nav />
+        <div className="App">
+          <Switch>
+            <Route path="/tracks/new" exact component={TrackForm} />
+            <Route
+              path="/instrumentals"
+              exact
+              component={InstrumentalsContainer}
+            />
+            <Route path="/tracks" exact component={TracksContainer} />
+            <Route path="/" exact component={Home} />
+          </Switch>
         </div>
-        <Footer/>
+        <Footer />
       </Router>
-    )
+    );
   }
 }
-export default App
+export default App;
