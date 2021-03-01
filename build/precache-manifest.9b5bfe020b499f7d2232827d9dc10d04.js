b3564c7fe77ab1f78c0a6f9493b8539ca80749cf
vincentservio <vincentservio@gmail.com>
2021-03-01 00:41:27 -0500

    gh-pages

---
+++ build/precache-manifest.9b5bfe020b499f7d2232827d9dc10d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2faae4f112fb7a2f3742a26bb8b331",
+    "url": "/TrackBackers_Frontend/index.html"
+  },
+  {
+    "revision": "338fb92f6e9875b8140d",
+    "url": "/TrackBackers_Frontend/static/css/main.3cbbddea.chunk.css"
+  },
+  {
+    "revision": "5f3a26d36af5e0f0d8de",
+    "url": "/TrackBackers_Frontend/static/js/2.9d17bc7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TrackBackers_Frontend/static/js/2.9d17bc7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338fb92f6e9875b8140d",
+    "url": "/TrackBackers_Frontend/static/js/main.78a3c31b.chunk.js"
+  },
+  {
+    "revision": "6cb94584634db9627473",
+    "url": "/TrackBackers_Frontend/static/js/runtime-main.7a7be563.js"
+  }
+]);
